7ff756184937a5a8a51c82d1983116c94d7c6814
sss <sujianfu199@qq.com>
2019-06-17 14:32:13 +0800

    xcode工程初始化

---
+++ STools/STools.h
@@ -0,0 +1,19 @@
+//
+//  STools.h
+//  STools
+//
+//  Created by SSS on 2019/6/17.
+//  Copyright © 2019 SSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STools.
+FOUNDATION_EXPORT double SToolsVersionNumber;
+
+//! Project version string for STools.
+FOUNDATION_EXPORT const unsigned char SToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STools/PublicHeader.h>
+
+
