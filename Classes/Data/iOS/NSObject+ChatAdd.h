25eb4243fd07eacbefb280777768feebc57f180b
sss <sujianfu199@qq.com>
2019-06-26 14:40:19 +0800

    1.chat目录部分的脚本没有加入到工程中,重新加入

--- Classes/Data/iOS/NSObject+ChatAdd.h
+++ Classes/Data/iOS/NSObject+ChatAdd.h
@@ -12,22 +12,6 @@
 
 @interface NSObject (ChatAdd)
 
-#if __APPLE__
-#if TARGET_IPHONE_SIMULATOR
-// iOS Simulator
-/**
- 根据字符串 限制宽度 字号 行距  纵间距 得到自适应面积
- 
- @param string 字符串
- @param width 最大宽度
- @param font 字号
- @param spacing 行距
- @param row 纵间距
- @return 返回rect
- */
-+(CGRect)getRectWith:(NSString *)string width:(CGFloat)width font:(UIFont *)font spacing:(CGFloat)spacing Row:(CGFloat)row;
-
-#elif TARGET_OS_IPHONE
 // iOS device
 /**
  根据字符串 限制宽度 字号 行距  纵间距 得到自适应面积
@@ -40,15 +24,6 @@
  @return 返回rect
  */
 +(CGRect)getRectWith:(NSString *)string width:(CGFloat)width font:(UIFont *)font spacing:(CGFloat)spacing Row:(CGFloat)row;
-
-
-#elif TARGET_OS_MAC
-
-// Other kinds of Mac OS
-#else
-#   //error "Unknown Apple platform"
-#endif
-#endif
 
 
 /**
